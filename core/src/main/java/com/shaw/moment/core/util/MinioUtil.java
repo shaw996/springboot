2230a626a5b59c4920dd449860b17db548cf48d3
Shaw996 <shawliu996@gmail.com>
2024-10-27 21:37:25 +0800

    fix: 🐛 修复上传图片后url错误问题

--- core/src/main/java/com/shaw/moment/core/util/MinioUtil.java
+++ core/src/main/java/com/shaw/moment/core/util/MinioUtil.java
@@ -11,7 +11,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -41,7 +40,7 @@ public class MinioUtil {
   MinioUtil(MinioConfig minioConfig, MinioClient minioClient) {
     this.minioConfig = minioConfig;
     this.minioClient = minioClient;
-    this.minioBaseUrl = minioConfig.getBaseUrl() + "/api/file/preview/";
+    this.minioBaseUrl = minioConfig.getBaseUrl();
   }
 
   /**

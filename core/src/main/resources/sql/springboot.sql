3e0b009f0f86fc7f7bb18f26537f0df87bbd9e75
Shaw996 <shawliu996@gmail.com>
2024-11-01 15:35:08 +0800

    feat: DDL

--- core/src/main/resources/sql/springboot.sql
+++ core/src/main/resources/sql/springboot.sql
@@ -12,12 +12,12 @@ USE `springboot`;
 -- ----------------------------
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
-  `id` VARCHAR(30) NOT NULL COMMENT '用户id',
+  `id` CHAR(30) NOT NULL COMMENT '用户id',
   `username` VARCHAR(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '用户名',
   `wechat_id` VARCHAR(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '微信id',
   `nickname` VARCHAR(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '用户昵称',
   `password` VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '用户密码',
-  `phone` VARCHAR(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '用户手机',
+  `phone` CHAR(11) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '用户手机',
   `email` VARCHAR(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '用户邮箱',
   `avatar` VARCHAR(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '用户头像',
   `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
@@ -30,5 +30,6 @@ CREATE TABLE `user` (
 -- Records of user
 -- ----------------------------
 INSERT INTO `user` (`id`, `username`, `wechat_id`, `nickname`, `password`, `phone`, `email`, `avatar`) VALUES ('1', 'test', 'testWechatId', '测试用户', '123456', '193xxxx5202', 'example@example.com', 'avatar.png');
+INSERT INTO `user` (`id`, `username`, `wechat_id`, `nickname`, `password`, `phone`, `email`, `avatar`) VALUES ('2', 'test2', 'test2WechatId', '测试用户2', '123456', '193xxxx5202', 'example@example.com', 'avatar.png');
 
 SET FOREIGN_KEY_CHECKS = 1;
